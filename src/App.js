bdeb0979b2dafb501748f57d0b8b2eb166f9a4c1
Mercado Facundo <mercadofacundo21@gmail.com>
2022-11-17 12:41:37 -0300

    Modificacion de estilos

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import Router from "./Shared/navigation/Router";
-import lightTheme from "./Shared/theme/LightTheme";
-/* import darkTheme from './Shared/theme/DarkTheme'; */
+import LightTheme from "./Shared/theme/LightTheme"
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
 
 const App = () => {
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={LightTheme}>
       <Toaster position="bottom-right" />
       <BrowserRouter>
         <Router />
